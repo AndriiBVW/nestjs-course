7b7182cf69b53bcf333479468a446f7adec59e76
Andrii Babenko <andriibvw@gmail.com>
2026-05-11 08:05:16 +0300

    feat: add CreateTaskDto and implement task creation in TaskService and TaskController

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('task')
 export class TaskController {
@@ -13,5 +14,10 @@ export class TaskController {
   @Get('by-id/:id')
   findById(@Param('id') id: string) {
     return this.taskService.findById(Number(id));
+  }
+
+  @Post()
+  create(@Body() dto: CreateTaskDto) {
+    return this.taskService.create(dto);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TaskService {
@@ -25,5 +26,16 @@ export class TaskService {
       throw new NotFoundException('Task not found');
     }
     return task;
+  }
+
+  create(dto: CreateTaskDto) {
+    const { title } = dto;
+    const newTask = {
+      id: this.tasks.length + 1,
+      title,
+      isComplete: false,
+    };
+    this.tasks.push(newTask);
+    return this.tasks;
   }
 }
